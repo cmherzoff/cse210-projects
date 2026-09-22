149a53f4665efe3fc14a799ca84c8e8f9d936b32
cmherzoff <her20028@byui.edu>
2026-09-22 11:41:48 -0600

    Added grade system

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letter;
+
+        Console.Write("What is your grade percentage? ");
+        grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+            {
+            letter = "A";
+            }
+        else if (grade >= 80)
+            {
+            letter = "B";
+            }
+        else if (grade >= 70)
+            {
+            letter = "C";
+            }
+        else if (grade >= 60)
+            {
+            letter = "D";
+            }
+        else
+            {
+            letter = "F";
+            }
+
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
